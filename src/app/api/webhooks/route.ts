af341acb1b7d7902668e3501a05874f79f50c15a
svei00 <79055634+svei00@users.noreply.github.com>
2025-01-10 12:45:10 -0800

    Fixing the MongoDB issue v2

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -19,7 +19,7 @@ export async function POST(req: Request): Promise<Response> {
   const wh = new Webhook(SIGNING_SECRET);
 
   // Get headers
-  const headerPayload = headers();
+  const headerPayload = await headers();
   const svix_id = headerPayload.get('svix-id');
   const svix_timestamp = headerPayload.get('svix-timestamp');
   const svix_signature = headerPayload.get('svix-signature');
