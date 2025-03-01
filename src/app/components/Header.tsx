1e052e4763d6f9e87f8a2b26949eae9ced1fce78
svei00 <79055634+svei00@users.noreply.github.com>
2025-02-28 20:44:34 -0800

    Revert the changes

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,63 +1,120 @@
 "use client";
-import React from "react";
+import React, { FC, useEffect, useState } from "react";
 import { Button, Navbar, TextInput } from "flowbite-react";
 import Link from "next/link";
 import { AiOutlineSearch } from "react-icons/ai";
-import { FaMoon } from "react-icons/fa";
+import { FaMoon, FaSun } from "react-icons/fa"; // Icons for themes
 import { usePathname } from "next/navigation";
+import { useTheme } from "next-themes";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import { dark, neobrutalism } from "@clerk/themes";
+import { useRouter, useSearchParams } from "next/navigation";
 
-export default function Header(): React.ReactElement {
-  const path = usePathname();
+const Header: FC = () => {
+  const path: string = usePathname();
+  const { theme, setTheme, systemTheme } = useTheme(); // Access systemTheme
+  const [mounted, setMounted] = useState(false);
+  const router = useRouter();
+  const [searchTerm, setSearchTerm] = useState("");
+  const searchParams = useSearchParams();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(searchParams);
+    urlParams.set("searchTerm", searchTerm);
+    const searchQuery = urlParams.toString();
+    router.push(`/search?${searchQuery}`);
+  };
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(searchParams);
+    const searchTermFromUrl = urlParams.get("searchTerm");
+    if (searchTermFromUrl) {
+      setSearchTerm(searchTermFromUrl);
+    }
+  }, [searchParams]);
+
+  // Ensure the component is mounted before rendering to prevent hydration mismatch
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null; // Prevent rendering on the server
+
+  // Determine the icon based on theme and system settings
+  const currentTheme = theme === "system" ? systemTheme : theme;
+
   return (
-    <Navbar className="border-b-2">
-      <Link
-        href="/"
-        className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"
-      >
-        <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
-          Cro{"\u0301"}nicas del Vejerete
-        </span>
-        Blog
-      </Link>
-      <form>
-        <TextInput
-          type="text"
-          placeholder="Search..."
-          rightIcon={AiOutlineSearch}
-          className="hidden lg:inline"
-        />
-      </form>
-      <Button className="w-12 h-10 lg:hidden" color="gray" pill>
-        <AiOutlineSearch />
-      </Button>
-      <div className="flex gap-2 md:order-2">
-        <Button className="w-12 h-10 hidden sm:inline" color="gray" pill>
-          <FaMoon />
-        </Button>
-        <Link href="/sign-in">
-          <Button gradientDuoTone="purpleToBlue" outline>
-            Sign In
+    <>
+      <Navbar className="border-b-2">
+        <Link
+          href="/"
+          className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"
+        >
+          <span className="px-2 py-1 bg-gradient-to-r from-purple-600 via-yellow-300 to-pink-500 rounded-lg text-white">
+            Crónicas del Vejerete
+          </span>
+          Blog
+        </Link>
+
+        <form onSubmit={handleSubmit}>
+          <TextInput
+            type="text"
+            placeholder="Buscar..."
+            rightIcon={AiOutlineSearch}
+            className="hidden lg:inline"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+        </form>
+
+        <div className="flex gap-2 md:order-2 items-center">
+          {/* Dark Mode Toggle Button */}
+          <Button
+            className="w-10 h-10"
+            color="gray"
+            pill
+            onClick={() =>
+              setTheme(currentTheme === "light" ? "dark" : "light")
+            }
+          >
+            {currentTheme === "light" ? <FaSun /> : <FaMoon />}
           </Button>
-        </Link>
-        <Navbar.Toggle />
-      </div>
-      <Navbar.Collapse>
-        <Link href="/">
-          <Navbar.Link active={path === "/"} as="div">
-            Home
-          </Navbar.Link>
-        </Link>
-        <Link href="/about">
-          <Navbar.Link active={path === "/about"} as="div">
-            About
-          </Navbar.Link>
-        </Link>
-        <Link href="/projects">
-          <Navbar.Link active={path === "/projects"} as="div">
-            Projects
-          </Navbar.Link>
-        </Link>
-      </Navbar.Collapse>
-    </Navbar>
+          <SignedIn>
+            <UserButton
+              appearance={{
+                baseTheme: theme === "light" ? neobrutalism : dark,
+              }}
+              userProfileUrl="/dashboard?tab=profile"
+            />
+          </SignedIn>
+          <SignedOut>
+            <Button gradientDuoTone="purpleToBlue" outline>
+              <SignInButton />
+            </Button>
+          </SignedOut>
+          <Navbar.Toggle />
+        </div>
+        <Navbar.Collapse>
+          <Link href="/">
+            <Navbar.Link active={path === "/"} as="div">
+              Home
+            </Navbar.Link>
+          </Link>
+          <Link href="/about">
+            <Navbar.Link active={path === "/about"} as="div">
+              Acerca
+            </Navbar.Link>
+          </Link>
+          <Link href="/projects">
+            <Navbar.Link active={path === "/projects"} as="div">
+              Proyectos
+            </Navbar.Link>
+          </Link>
+        </Navbar.Collapse>
+      </Navbar>
+    </>
   );
-}
+};
+
+export default Header;
