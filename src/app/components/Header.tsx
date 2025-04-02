39be1ee0e5721949ea00767bca39af4835ab13e8
svei00 <79055634+svei00@users.noreply.github.com>
2025-04-02 00:33:31 -0500

    Working ina fix of the GitHub Actions and make a change on Header to see if it works v2 and updated the .github/workflow/deploy.yml file

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -72,7 +72,7 @@ const Header: FC = () => {
           href="/"
           className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"
         >
-          <span className="px-2 py-1 bg-gradient-to-r from-purple-600 via-yellow-300 to-pink-500 rounded-lg text-white">
+          <span className="px-2 py-1 bg-gradient-to-r from-purple-600 via-yellow-300 to-orange-500 rounded-lg text-white">
             Crónicas del Vejerete
           </span>
           Blog
